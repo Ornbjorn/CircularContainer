1a4eb7e3712702e28879b76207cda5a02e1f1a53
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2024-07-28 00:07:33 +0900

    Rename CircularContainer.gd with snake_case

--- addons/circular_container/register.gd
+++ addons/circular_container/register.gd
@@ -4,9 +4,9 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	add_custom_type(
-		"CircularContainer", "Container", preload("CircularContainer.gd"), preload("icon.png")
+		"CircularContainer", "Container", preload("circular_container.gd"), preload("icon.png")
 	)
-	print(self, preload("CircularContainer.gd"))
+	print(self, preload("circular_container.gd"))
 
 
 func _exit_tree() -> void:
