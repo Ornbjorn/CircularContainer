1d9d2c528aa83dcd0df0f71586e1b4dc99d2d4e0
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2024-07-27 22:42:42 +0900

    Lint register.gd

--- addons/KOBUGE-games.CircularContainer/register.gd
+++ addons/KOBUGE-games.CircularContainer/register.gd
@@ -1,9 +1,13 @@
 @tool
 extends EditorPlugin
 
+
 func _enter_tree() -> void:
-	add_custom_type("CircularContainer", "Container", preload("CircularContainer.gd"), preload("icon.png"))
+	add_custom_type(
+		"CircularContainer", "Container", preload("CircularContainer.gd"), preload("icon.png")
+	)
 	print(self, preload("CircularContainer.gd"))
+
 
 func _exit_tree() -> void:
 	remove_custom_type("CircularContainer")
