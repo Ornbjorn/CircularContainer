eb35fa32482274c15c632055e6b9a53cba4d61bd
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2024-07-27 22:06:49 +0900

    Make register.gd typed

--- addons/KOBUGE-games.CircularContainer/register.gd
+++ addons/KOBUGE-games.CircularContainer/register.gd
@@ -1,9 +1,9 @@
 @tool
 extends EditorPlugin
 
-func _enter_tree():
+func _enter_tree() -> void:
 	add_custom_type("CircularContainer", "Container", preload("CircularContainer.gd"), preload("icon.png"))
 	print(self, preload("CircularContainer.gd"))
 
-func _exit_tree():
+func _exit_tree() -> void:
 	remove_custom_type("CircularContainer")
